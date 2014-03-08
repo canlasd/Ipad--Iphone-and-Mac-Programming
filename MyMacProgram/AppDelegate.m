967e4b95d9992db45acc7c1c82ee58ac4f9666e2
David Canlas <davidcanlas@Davids-Mac-mini.local>
2014-03-07 20:29:08 -0500

    Initial Commit

---
+++ MyMacProgram/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MyMacProgram
+//
+//  Created by David Canlas on 3/7/2014.
+//  Copyright (c) 2014 David Canlas. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ MyMacProgram/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MyMacProgram
+//
+//  Created by David Canlas on 3/7/2014.
+//  Copyright (c) 2014 David Canlas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
